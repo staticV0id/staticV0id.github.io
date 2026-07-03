d8151f315ee3ab6dc9cf378e8cb1c74bc08d1dea
staticV0id <davidbimmel@gmail.com>
2026-07-03 14:36:49 +0200

    scroll fix

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-192.png',
-  './icons/icon-maskable-512.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png',
   './icons/apple-touch-icon.png'
 ];
 
